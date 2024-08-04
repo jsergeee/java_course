35db933e5a469a5f94dcfd766046e609f4e3dc5e
Sergey_Valerievich <163644503+jsergeee@users.noreply.github.com>
2024-08-04 14:54:39 +0300

    task1

---
+++ task1.java
@@ -0,0 +1,57 @@
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class task1 {
+    public static void main(String[] args){
+//   User u = new User("Serg", 47);
+//   System.out.println(u.getName() + u.getAge());
+//        // Сотрировка списка
+//        List<Integer> ints = List.of(16,23,3,46,5,6,67,8,-9);
+//        System.out.println(ints);
+//        List<Integer> i = new ArrayList<>();
+//        i.addAll(ints);
+//        Collections.sort(i);
+//        System.out.println(i);
+
+//        // Вывод даты и изменение формата
+
+//        LocalDateTime now = LocalDateTime.now();
+//        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//        DateTimeFormatter sdf = DateTimeFormatter.ofPattern("dd.MMMM.yyyy HH:mm:ss"); // оборажение месяца завист от количества MM
+//
+//
+//        String formatedDate1 = now.format(df);
+//        String formatedDate2 = now.format(sdf);
+//
+//        System.out.println("Формат по умолчанию: " + formatedDate1);
+//        System.out.println("(Преобразованный формат: " + formatedDate2);
+
+
+
+
+    }
+
+}
+// Геттер
+
+//class User{
+//    String name;
+//    int age;
+//
+//    public User(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//}
+
+
